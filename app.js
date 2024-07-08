916623773e67061f484b25a02190df2eff78a69a
Cristhian Castro Arias <cristhiancastro001@gmail.com>
2024-07-07 21:05:17 -0500

    2do modulo

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 // desafios https://github.com/alura-es-cursos/js-curso/blob/main/1.md
+// document es un puente entre js y html, h1 es la etiqueta. titulo seria una variable de tipo header
 
-let titulo = document.querySelector("h1"); // document es un puente entre js y html, h1 es la etiqueta. titulo seria una variable de tipo header
-titulo.innerHTML = "Juego de numero secreto";
+// let parrafo = document.querySelector("p");
+// parrafo.innerHTML = "Escoge un numero de 1 al 10"
 
-let parrafo = document.querySelector("p");
-parrafo.innerHTML = "Escoge un numero de 1 al 10"
+// function intentoDeUsuario() { // muestra pop up con aviso cuando da click en boton "intentar"
+//    alert("Click desde boton");
+//    return; 
+// }
 
-function intentoDeUsuario() {
-    alert("Click desde boton");
+let numeroSecreto = generarNumeroSecreto();
+
+function asignarTextoElemento (elemento, texto){
+    let elementoHTML = document.querySelector(elemento); 
+    elementoHTML.innerHTML = texto;
+    return; // no es necesario, pero es buena practica
 }
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value); // es como queryselector pero usa el id. valorUsuario es un id de la etiqueta input
+    console.log(typeof(numeroDeUsuario));
+    console.log(numeroSecreto);
+    console.log(numeroDeUsuario);
+    console.log(numeroDeUsuario == numeroSecreto); // da un booleano
+    return; 
+}
+
+function generarNumeroSecreto(){
+    return Math.floor(Math.random()*10)+1;
+
+}
+
+asignarTextoElemento ("h1", "Juego de numero secreto");
+asignarTextoElemento ("p", "Escoge un numero de 1 al 10");
